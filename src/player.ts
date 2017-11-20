85c8fd4bceb1da80293ee43e448a1f2c4083775b
Ethan Barker <ethanbarker0@gmail.com>
2017-11-20 00:03:09 -0800

    Init

---
+++ src/player.ts
@@ -0,0 +1,27 @@
+import {Vector2} from "./vector2";
+
+export class Player {
+    get position(): Vector2 {
+        return this._position;
+    }
+
+    set position(value: Vector2) {
+        this._position = value;
+    }
+    private size: number;
+    private _position: Vector2;
+
+    constructor() {
+        this.size = 20;
+        this._position = new Vector2(0, 0);
+    }
+
+    public addSize(amount: number) {
+        this.size += amount;
+    }
+
+    public getSize(): number {
+        return this.size;
+    }
+
+}
---
+++ tests/tests.ts
@@ -0,0 +1,27 @@
+import chai = require('chai');
+import mocha = require('mocha');
+import {Player} from "../src/player";
+import {Vector2} from "../src/vector2";
+
+mocha.describe('Player', function() {
+
+    mocha.it('A Player should be of 20 size on instantiation', function() {
+        let player = new Player();
+        chai.expect(player.getSize()).to.equal(20);
+        chai.expect(player.getSize()).to.be.a("number");
+    });
+
+    mocha.it('A Player should have a position of (0, 0)', function() {
+        let player = new Player();
+        chai.expect(player.position.x).to.equal(0);
+        chai.expect(player.position.y).to.equal(0);
+    });
+});
+
+mocha.describe('Vector2', function() {
+    mocha.it('A vector should be of (0, 0) on instantiation', function() {
+        let vector = new Vector2();
+        chai.expect(vector.x).to.equal(0);
+        chai.expect(vector.y).to.equal(0);
+    });
+});
